c13d43fc10a5335be2a5f8d63984eac9e90675cf
Samuel Bernhardt <sdbernhardt@gmail.com>
2019-08-07 21:58:03 -0400

    Added unicode conversion funciton

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
-# Text Style Slack App
+# Text Styles - Slack App
 
 https://api.slack.com/apps/AM4EK1KL0
 http://127.0.0.1:4040/inspect/http
 https://www.npmjs.com/package/to-unicode
 https://qaz.wtf/u/convert.cgi?text=abcdefghijklmnopqrstuvwxyz
-
 
 Future references:
 http://slothsoft.net/UnicodeMapper/?lang=en-us
---
+++ helpers/toUnicode.js
@@ -0,0 +1,36 @@
+var {styles} = require("../styles");
+
+var standard = "abcdefghijklmnopqrstuvwxyz".split("");
+
+var string = "hello";
+
+// console.log(styles);
+
+function toUnicode(string, style) {
+  var result = "";
+  var styleAlphabet = styles[style];
+
+  for (var i = 0; i < string.length; i++) {
+      var letter = string.charAt(i);
+      var index = standard.indexOf(letter);
+
+      if (index >= 0) {
+        if (styleAlphabet.length == 52) {
+          result += getLetter52(styleAlphabet, index)
+        } else if (styleAlphabet.length == 26){
+          result += styleAlphabet[index];
+        }
+      } else {
+        result += letter;
+      }
+  }
+
+  return result;
+}
+
+console.log(toUnicode("hello! how are you?", "Superscript"));
+
+function getLetter52(alphabet, index) {
+  index = index * 2;
+  return alphabet[index] + alphabet[index + 1];
+}
