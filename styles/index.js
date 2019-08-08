8ae777e1bf98690b785f368d2aa123d363944f27
Samuel Bernhardt <sdbernhardt@gmail.com>
2019-08-07 21:11:03 -0400

    edited code to now use abstracted methods that live in the helpers folder

---
+++ styles/index.js
@@ -0,0 +1,26 @@
+exports.styles = [
+  {
+    name: "Circle",
+    alphabet: "ⓐⓑⓒⓓⓔⓕⓖⓗⓘⓙⓚⓛⓜⓝⓞⓟⓠⓡⓢⓣⓤⓥⓦⓧⓨⓩ"
+  },
+  {
+    name: "Circle Negative",
+    alphabet: "🅐🅑🅒🅓🅔🅕🅖🅗🅘🅙🅚🅛🅜🅝🅞🅟🅠🅡🅢🅣🅤🅥🅦🅧🅨🅩"
+  },
+  {
+    name: "Full Width",
+    alphabet: "ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ"
+  },
+  {
+    name: "Gothic",
+    alphabet: "𝖆𝖇𝖈𝖉𝖊𝖋𝖌𝖍𝖎𝖏𝖐𝖑𝖒𝖓𝖔𝖕𝖖𝖗𝖘𝖙𝖚𝖛𝖜𝖝𝖞𝖟"
+  },
+  {
+    name: "Script",
+    alphabet: "𝓪𝓫𝓬𝓭𝓮𝓯𝓰𝓱𝓲𝓳𝓴𝓵𝓶𝓷𝓸𝓹𝓺𝓻𝓼𝓽𝓾𝓿𝔀𝔁𝔂𝔃"
+  },
+  {
+    name: "Superscript",
+    alphabet: "ᵃᵇᶜᵈᵉᶠᵍʰⁱʲᵏˡᵐⁿᵒᵖqʳˢᵗᵘᵛʷˣʸᶻ"
+  }
+];
